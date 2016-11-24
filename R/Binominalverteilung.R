4c999a0923d6f3e0b923d61bb601eecdb524ea5c
dominikpeter <dominik.peter87@gmail.com>
2016-11-24 21:52:21 +0100

    plt

--- R/Binominalverteilung.R
+++ R/Binominalverteilung.R
@@ -19,9 +19,11 @@ df[, `:=` (relative_häufigkeit = unlist(lapply(.SD$sample, function(x) mean(x))
            häufigkeit = unlist(lapply(.SD$sample, function(x) sum(x))))]
 
 # Histogramm der Verteilung
-df$relative_häufigkeit %>% hist
+df$relative_häufigkeit %>% hist(main ="Histogramm der relativen Häufigkeiten")
 
 # Anzahl relative und absolute Häufigkeit
 df[, .N, by = .(häufigkeit, relative_häufigkeit)][order(-N)]
 
 # 101 wird kein Kopf geworfen und 96 wird nur Kopf geworfen, wobei 24519mal 5 Mal Kopf und 5 Mal Zahl geworfen wird
+
+
