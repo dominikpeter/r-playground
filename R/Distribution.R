fa1f2f67142c78dfd50accc79ea01daa07ee6067
dominikpeter <dominik.peter87@gmail.com>
2016-11-24 21:41:04 +0100

    binominalverteilung

---
+++ R/Distribution.R
@@ -0,0 +1,23 @@
+library(magrittr)
+
+set.seed(2323)
+
+
+library(data.table)
+
+# Simulation von 100000 Versuchen
+# -------------------------------------------------------------------------------------------------------------------
+n <- 1:100000
+#es werden 10 Mal eine Münze mit Kopf oder Zahl geworfen
+df <- data.table(n) %>% .[, sample := list(lapply(1:100000, function(x) rbinom(10, 1, 0.5)))] 
+
+# für jeden Versuch wird die relative Häufigkeit von Kopf berechnet
+df[, `:=` (relative_häufigkeit = unlist(lapply(.SD$sample, function(x) mean(x))),
+           häufigkeit = unlist(lapply(.SD$sample, function(x) sum(x))))]
+# Histogramm
+df$relative_häufigkeit %>% hist
+
+# Anzahl 
+df[, .N, by = .(häufigkeit, relative_häufigkeit)][order(-N)]
+
+
