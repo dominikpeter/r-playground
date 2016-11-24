98b62aa9decfb16459f15ba0731346b95a91394b
dominikpeter <dominik.peter87@gmail.com>
2016-11-24 22:49:20 +0100

    bugs

--- R/Binominalverteilung.R
+++ R/Binominalverteilung.R
@@ -61,6 +61,6 @@ verteilung <- df[, .N, by = .(häufigkeit, relative_häufigkeit)][order(-N)]
 
 verteilung[häufigkeit == 4, N] / sum(verteilung[, N])
 
-# Lösungsansatz während des unterrichgts
+# Lösungsansatz während des Unterrichts / gleiche Lösung bei vielen Versuchen!
 dbinom(4, size=12, prob=0.2) 
 
