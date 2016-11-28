133490970f87d9f5a749860ff45ceedf9eb2d3fc
Peter Dominik <PeterD@crhsd.local>
2016-11-28 07:58:17 +0100

    i

--- R/Binominalverteilung.R
+++ R/Binominalverteilung.R
@@ -29,19 +29,18 @@ df %>% ggplot(aes(x=häufigkeit)) +
   ylab("P(X = k)") + xlab("k")
   
 
-
 # Anzahl relative und absolute Häufigkeit
 df[, .N, by = .(häufigkeit, relative_häufigkeit)][order(-N)]
 
 # 101 wird kein Kopf geworfen und 96 wird nur Kopf geworfen, wobei 24519mal 5 Mal Kopf und 5 Mal Zahl geworfen wird
 
 
-
 # Beispiel Prüfung
 # -------------------------------------------------------------------------------------------------------------------
 # Suppose there are twelve multiple choice questions in an English class quiz.
 # Each question has five possible answers, and only one of them is correct.
-# Find the probability of having four or less correct answers if a student attempts to answer every question at random.
+# Find the probability of having four or less correct answers if a student attempts
+# to answer every question at random.
 
 df <- NULL
 n <- 1:1000000
@@ -52,8 +51,6 @@ df <- data.table(n) %>% .[, sample := .(lapply(n, function(x) rbinom(12, 1, 1/5)
 # für jeden Versuch wird die relative Häufigkeit von Kopf berechnet
 df[, `:=` (relative_häufigkeit = sapply(.SD$sample, mean),
            häufigkeit = sapply(.SD$sample, sum))]
-
-
 
 
 df %>% ggplot(aes(x=häufigkeit)) +
