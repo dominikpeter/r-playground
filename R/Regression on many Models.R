3c9923b0f60f2c23603b5c8066b2b7841c16b902
dominikpeter <dominik.peter87@gmail.com>
2016-11-29 22:35:40 +0100

    clean code

--- R/Regression on many Models.R
+++ R/Regression on many Models.R
@@ -21,13 +21,21 @@ get_slope <- function(df) {
   df$estimate[2]
 }
 
-by_group[, model := lapply(data, reg)]
-by_group[, tidy := lapply(model, broom::tidy)]
-by_group[, slope := vapply(tidy, get_slope, double(1))]
+by_group %>% 
+  .[, model := lapply(data, reg)] %>%
+  .[, tidy := lapply(model, broom::tidy)] %>% 
+  .[, glance := lapply(model, broom::glance)] %>% 
+  .[, slope := vapply(tidy, `[`, double(1), 2, 2)] %>% 
+  .[, r.squared := vapply(glance, `[[`, double(1), "r.squared")]
 
 
-by_group[slope > quantile(slope, 0.01)] %>% 
+by_group[slope > quantile(slope, 0.01) & continent != "Oceania"] %>% 
   ggplot(aes(y=slope, x=continent)) +
+  geom_boxplot(fill="#16a085", alpha = 3/5)
+
+
+by_group[r.squared > quantile(r.squared, 0.01) & continent != "Oceania"] %>% 
+  ggplot(aes(y=r.squared, x=continent)) +
   geom_boxplot(fill="#16a085", alpha = 3/5)
 
 
